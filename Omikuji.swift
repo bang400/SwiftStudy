08a1b737d4d965a5ef94c922f2e8dbc22fe021bb
bangbang400 <bangbang400cc@gmail.com>
2022-03-26 21:27:32 +0900

    Omikujiコードを作成

---
+++ Omikuji.swift
@@ -0,0 +1,31 @@
+import Foundation
+// Run:OnlineSwiftPlayground
+// おみくじコード
+
+// ランダム関数
+func makeRand() -> Int {
+    let randInt = Int.random(in: 1...5)
+    return randInt
+}
+
+// おみくじ判定関数
+func judgeMent(_ param:Int) -> String {
+    let num : Int = param
+    var result: String  = ""
+
+    switch num {
+        case 1:
+        result = "大吉"
+        case 2:
+        result = "中吉"
+        case 3:
+        result = "小吉"
+        //case 4:
+        //print("凶")
+        default:
+        result = "末吉"        
+    }
+    return result
+}
+
+print(judgeMent(makeRand()))
